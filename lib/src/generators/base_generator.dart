12eebc8081ffb1382c16bff1709ef4985d2d7a5b
Bony <bonyyamin1997@gmail.com>
2026-05-07 19:51:06 +0600

    feat: add BaseGenerator abstract class

--- lib/src/generators/base_generator.dart
+++ lib/src/generators/base_generator.dart
@@ -1,1 +1,36 @@
- 
+abstract class BaseGenerator {
+  final FlowConfig config;
+  final TemplateEngine engine;
+  final AppLogger logger;
+
+  BaseGenerator({
+    required this.config,
+    required this.engine,
+    required this.logger,
+  });
+
+  String get name;
+  List<Dependency> get requiredDependencies;
+  Future<List<GeneratedFile>> generate();
+
+  // Convenience: render template and return GeneratedFile
+  Future<GeneratedFile> renderToFile({
+    required String templatePath,
+    required String outputPath,
+    Map<String, dynamic> extraVars = const {},
+    FileType type = FileType.dart,
+  }) async {
+    final content = await engine.render(
+      templatePath,
+      {...config.toTemplateVars(), ...extraVars},
+    );
+    return GeneratedFile(
+      relativePath: outputPath,
+      content: content,
+      type: type,
+    );
+  }
+
+  // Shorthand for the state management folder name
+  String get sm => config.stateManagement.folderName;
+}
