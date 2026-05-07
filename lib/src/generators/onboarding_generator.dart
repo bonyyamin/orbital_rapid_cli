901f84132a67d488d28c218b5c851efdae127fa2
Bony <bonyyamin1997@gmail.com>
2026-05-07 21:18:57 +0600

    feat: add splash and onboarding generator

--- lib/src/generators/onboarding_generator.dart
+++ lib/src/generators/onboarding_generator.dart
@@ -1,1 +1,77 @@
+import 'package:orbital_rapid_cli/src/generators/base_generator.dart';
+import 'package:orbital_rapid_cli/src/models/dependency.dart';
+import 'package:orbital_rapid_cli/src/models/generated_file.dart';
+
+class OnboardingGenerator extends BaseGenerator {
+  OnboardingGenerator({
+    required super.config,
+    required super.engine,
+    required super.logger,
+  });
+
+  @override
+  String get name => 'Onboarding';
+
+  @override
+  List<Dependency> get requiredDependencies => [];
+
+  @override
+  Future<List<GeneratedFile>> generate() async {
+    final files = <GeneratedFile>[
+      await renderToFile(
+        templatePath: 'templates/onboarding/$sm/onboarding_screen.dart.tmpl',
+        outputPath: 'lib/features/onboarding/presentation/onboarding_screen.dart',
+      ),
+      await renderToFile(
+        templatePath: 'templates/onboarding/$sm/widgets/onboarding_page.dart.tmpl',
+        outputPath: 'lib/features/onboarding/presentation/widgets/onboarding_page.dart',
+      ),
+      await renderToFile(
+        templatePath: 'templates/onboarding/$sm/widgets/page_indicator.dart.tmpl',
+        outputPath: 'lib/features/onboarding/presentation/widgets/page_indicator.dart',
+      ),
+    ];
+
+    if (config.isRiverpod) {
+      files.addAll([
+        await renderToFile(
+          templatePath: 'templates/onboarding/riverpod/onboarding_provider.dart.tmpl',
+          outputPath: 'lib/features/onboarding/presentation/riverpod/onboarding_provider.dart',
+        ),
+        await renderToFile(
+          templatePath: 'templates/onboarding/riverpod/onboarding_state.dart.tmpl',
+          outputPath: 'lib/features/onboarding/presentation/riverpod/onboarding_state.dart',
+        ),
+      ]);
+    }
+
+    if (config.isBloc) {
+      files.addAll([
+        await renderToFile(
+          templatePath: 'templates/onboarding/bloc/onboarding_bloc.dart.tmpl',
+          outputPath: 'lib/features/onboarding/presentation/bloc/onboarding_bloc.dart',
+        ),
+        await renderToFile(
+          templatePath: 'templates/onboarding/bloc/onboarding_event.dart.tmpl',
+          outputPath: 'lib/features/onboarding/presentation/bloc/onboarding_event.dart',
+        ),
+        await renderToFile(
+          templatePath: 'templates/onboarding/bloc/onboarding_state.dart.tmpl',
+          outputPath: 'lib/features/onboarding/presentation/bloc/onboarding_state.dart',
+        ),
+      ]);
+    }
+
+    if (config.isGetX) {
+      files.add(
+        await renderToFile(
+          templatePath: 'templates/onboarding/getx/onboarding_controller.dart.tmpl',
+          outputPath: 'lib/features/onboarding/presentation/getx/onboarding_controller.dart',
+        ),
+      );
+    }
+
+    return files;
+  }
+}
  
--- lib/src/generators/splash_generator.dart
+++ lib/src/generators/splash_generator.dart
@@ -1,1 +1,56 @@
+import 'package:orbital_rapid_cli/src/generators/base_generator.dart';
+import 'package:orbital_rapid_cli/src/models/dependency.dart';
+import 'package:orbital_rapid_cli/src/models/generated_file.dart';
+
+class SplashGenerator extends BaseGenerator {
+  SplashGenerator({
+    required super.config,
+    required super.engine,
+    required super.logger,
+  });
+
+  @override
+  String get name => 'Splash Screen';
+
+  @override
+  List<Dependency> get requiredDependencies => [];
+
+  @override
+  Future<List<GeneratedFile>> generate() async {
+    final files = <GeneratedFile>[
+      await renderToFile(
+        templatePath: 'templates/splash/$sm/splash_screen.dart.tmpl',
+        outputPath: 'lib/features/splash/presentation/splash_screen.dart',
+      ),
+    ];
+
+    if (config.isBloc) {
+      files.addAll([
+        await renderToFile(
+          templatePath: 'templates/splash/bloc/splash_bloc.dart.tmpl',
+          outputPath: 'lib/features/splash/presentation/bloc/splash_bloc.dart',
+        ),
+        await renderToFile(
+          templatePath: 'templates/splash/bloc/splash_event.dart.tmpl',
+          outputPath: 'lib/features/splash/presentation/bloc/splash_event.dart',
+        ),
+        await renderToFile(
+          templatePath: 'templates/splash/bloc/splash_state.dart.tmpl',
+          outputPath: 'lib/features/splash/presentation/bloc/splash_state.dart',
+        ),
+      ]);
+    }
+
+    if (config.isGetX) {
+      files.add(
+        await renderToFile(
+          templatePath: 'templates/splash/getx/splash_controller.dart.tmpl',
+          outputPath: 'lib/features/splash/presentation/getx/splash_controller.dart',
+        ),
+      );
+    }
+
+    return files;
+  }
+}
  
